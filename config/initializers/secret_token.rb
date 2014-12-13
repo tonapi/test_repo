d9199d22b9026425f6c1c5dcd9eb7b8ff99fe3d7
tonapi <tnapiorkowski@poczta.fm>
2014-12-13 16:59:09 +0100

    created Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testgit::Application.config.secret_key_base = '1576368478b82a1bb012d5569c74b3e85936e44e2b5ad17042d2a8bedc410d7b46e2a226483cb9463a792209760d1fb76722df2b87bd50f255fa7599f563f9fd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Testgit::Application.config.session_store :cookie_store, key: '_testgit_session'
